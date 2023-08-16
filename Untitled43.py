afdcf2ce1cdcabb33d72ba97c991e087e25aa05b
Honneurr <140384402+Honneurr@users.noreply.github.com>
2023-08-16 13:15:04 +0300

    Add files via upload

---
+++ Untitled43.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+lass Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        arr = ListNode()  
+        current = arr
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+        
+        return arr.next
+
